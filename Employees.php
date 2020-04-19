651201e0eb8399277be1eb006bbce3842601b72f
macbookpro <mcneerta@vcu.edu>
2020-04-19 00:56:05 -0400

    Changed employee file to just IDs

--- Employees.php
+++ Employees.php
@@ -2,15 +2,15 @@
 
 require_once('Connection.php');
 
-$stmt = $conn->prepare("SELECT employee_id, concat(first_name, ', ', last_name) as "Full Name" from employees);
+$stmt = $conn->prepare("SELECT employee_id from employees);
 $stmt->execute();
 
 echo "<table style= 'border: solid 1px black;'> ";
-echo "<thead><tr><th>ID</th><th>Full Name</th></tr></thead>";
+echo "<thead><tr><th>ID</th></tr></thead>";
 echo "<tbody>";
 
 while ($row = $stmt->fetch()){
-       echo "<tr><td>$row[employee_id]</td><td>$row[Full Name]</td><tr>";
+       echo "<tr><td>$row[employee_id]</td></tr>";
 }
 
 echo "</tbody>";
