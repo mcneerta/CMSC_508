f64cc88b76055232aa7d00dc16a7b337fab5d79e
macbookpro <mcneerta@vcu.edu>
2020-04-19 01:49:48 -0400

    Adding index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>CMSC 508</title>
+</head>
+<body>
+<ul>
+<li><a href="Employees.php">List all employees</a></li>
+<li><a href="Info.php">List PHP info</a></li>
+<li><a href="Test.php">Say Hello!</a></li>
+</ul>
+</body>
+</html>
