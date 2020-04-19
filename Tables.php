81a0477c9d8388e9a51f3f8b527fd121686bb798
macbookpro <mcneerta@vcu.edu>
2020-04-19 01:23:08 -0400

    Adding test employee table

---
+++ Tables.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once('Connection.php');
+
+try {
+
+$sql = "CREATE TABLE employees(
+employee_id INT(6) PRIMARY KEY
+)";
+
+$entry = "INSERT INTO employees VALUES(10)";
+
+$conn->execute($sql);
+echo "Table created successfully";
+
+$conn->exec($entry);
+echo "New record created successfully";
+}
+
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+}
+
+?>
