641b785775e7b19de4463154c588e6fd516b83f9
macbookpro <mcneerta@vcu.edu>
2020-04-20 02:19:53 -0400

    Updated index file

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 </head>
 <body>
 <ul>
-<li><a href="Employees.php">List all employees</a></li>
-<li><a href="Info.php">List PHP info</a></li>
-<li><a href="Test.php">Say Hello!</a></li>
 <li><a href="login.php">Login here!</a></li>
 <li><a href="register.php">Register for a new account!</a></li>
 </ul>
