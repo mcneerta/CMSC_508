b6b86d75ee4610ade140ebe62a2d11deafe1f6e1
macbookpro <mcneerta@vcu.edu>
2020-04-20 01:55:27 -0400

    Adding register file to index

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <li><a href="Info.php">List PHP info</a></li>
 <li><a href="Test.php">Say Hello!</a></li>
 <li><a href="login.php">Login here!</a></li>
-<li><a href="Test.php">Register for a new account!</a></li>
+<li><a href="register.php">Register for a new account!</a></li>
 </ul>
 </body>
 </html>
