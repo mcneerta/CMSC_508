edc7be7cb9966112c65e17b1e30be3021431634d
macbookpro <mcneerta@vcu.edu>
2020-04-20 02:21:32 -0400

    Commented out automatic login

--- login.php
+++ login.php
@@ -3,11 +3,11 @@
 session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+/*if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: Test.php");
     exit;
 }
- 
+ */
 // Include config file
 require_once "Connection.php";
  
