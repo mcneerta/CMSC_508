b381c8bcad84a611d8e4d0045e6aed8517bb46e8
macbookpro <mcneerta@vcu.edu>
2020-05-03 00:23:37 -0400

    Debuging leaderboard display

--- user_leaderboard.php
+++ user_leaderboard.php
@@ -26,8 +26,7 @@ WHERE (SELECT IFNULL(SUM(b.points_earned),0) FROM tbl_completedquest b WHERE b.p
         RIGHT JOIN tbl_player b
         ON a.user_id = b.player_id
          AND a.login_id = c.login_id
-    WHERE (SELECT IFNULL(SUM(d.points_earned),0) FROM tbl_completedquest d WHERE d.player_id 
-                    = (SELECT z.user_id FROM tbl_user z WHERE z.login_id = :login_id) )
+    WHERE (SELECT IFNULL(SUM(d.points_earned),0) FROM tbl_completedquest d) 
             <=
             (SELECT IFNULL(SUM(e.points_earned),0) FROM tbl_completedquest e WHERE e.player_id 
                     = a.user_id)
