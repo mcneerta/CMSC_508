976df8230d4299181f5848361fe6e29bc7454af5
macbookpro <mcneerta@vcu.edu>
2020-05-03 22:12:06 -0400

    Reverting style of user messages

--- messages.php
+++ messages.php
@@ -130,26 +130,27 @@ if ($stmt = $conn->prepare("SELECT title FROM tbl_chatroom WHERE chatroom_id = :
                     $counter = 0;
                     while ($row = $stmt->fetch()) {
                         if($row['user_id'] == $user_id){
-                            echo "<tr>";
-                            echo "<td align='center'>";
-                            echo "<button class='container' type='submit' name='look_message' value='' >";
+                            echo "<div class='container' role='button' onclick='$(this).closest('form').submit()' >";
+                            echo "<div class='container_message darker con_right' >";
+                            echo "<div>";
                             echo "<p class='right username'>".$row['username']."</p>";
+                            echo "</div>";
+                            echo "<div>";
                             echo "<p class='right'>".$row['message']."</p>";
                             echo "<span class='time-right'>".$row['time_stamp']."</span>";
-                            echo "</button>";
-                            echo "</td>";
-                            echo "</tr>";
+                            echo "</div>";
+                            echo "</div>";
+                            echo "</div>";
+
 
                         } else {
-                            echo "<tr>";
-                            echo "<td align='center'>";
-                            echo "<button class='container' type='submit' name='look_message' value='' >";
-                            echo "<p class='right username'>".$row['username']."</p>";
-                            echo "<p class='right'>".$row['message']."</p>";
+                            echo "<div class='container'>";
+                            echo "<div class='container_message' >";
+                            echo "<p class='username'>" . $row['username'] . "</p>";
+                            echo "<p>" . $row['message'] . "</p>";
                             echo "<span class='time-left'>".$row['time_stamp']."</span>";
-                            echo "</button>";
-                            echo "</td>";
-                            echo "</tr>";
+                            echo "</div>";
+                            echo "</div>";
                         }
                         $counter += 1;
                     }
